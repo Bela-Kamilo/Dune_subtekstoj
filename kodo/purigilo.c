64fd79a5c4f3de866b3733aac75621fec4cc5824
sathon <sathon@hostname1.home>
2025-10-05 18:44:59 +0300

    Kodo kiun mi uzis

--- kodo/purigilo.c
+++ kodo/purigilo.c
@@ -1,27 +1,24 @@
-
-
 // ricevas .srt dosieron
 // produktas puran.srt dosieron havante la tempopunktoj  sen la teksto
-
 
 
 // aserto : la dosiero ekzistos
 // aserto : la dosiero sekvos la .srt formaton
 
-
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 
+#define DIRTY_FILE_USES_WIN_NEWLINES 1
+
+char fgetc2(FILE* file);
+
 int main(int argc, char **argv){
-
-
 	char sourcefile[100]={};
 	char c;
 	int i=0;
 
 	enum two_newline_finder_fsm {none, one,two};
-	
 	enum two_newline_finder_fsm fsm ;
 
 	if(argc <1) {
@@ -31,81 +28,63 @@ int main(int argc, char **argv){
 	else
 		strcpy(sourcefile ,argv[1]);
 
-
-
 	FILE* dirty =fopen(sourcefile , "r");
 	FILE* clean = fopen("pura.srt" ,"w");
 
-
 	if(!dirty) {printf("dosiero \"%s\" ne trovebla\n",sourcefile);exit(-1);}
+	if(!clean) {printf("ne povas krei dosieron\n");exit(-1);}
 
-	//la formato estas
-	
+	//la formato estas	
 	//subtekstonumero -newline 1-
 	//temposignoj     -newline 2-
 	// teksto	-newline3--newline4-
-	
-	
 	while(1){
-		
-		
 		//legi numeron
 		//verki numeron
 		do{
-			
-			c=fgetc(dirty);
+			c=fgetc2(dirty);
 			fputc(c,clean);
-		
 		}while(c!='\n');
-		
-		
 	
 		//legi temposignojn
 		//verki temposignojn
-		
 		do{
-			c=fgetc(dirty);
+			c=fgetc2(dirty);
 			fputc(c,clean);
-			
 		}while(c!='\n');
 		
-
 		//legi tekston 
 		//ignori tekston
 		//verki -newline-on
-		
 		fsm=none;
-		
 		while(fsm!=two){
-		
-			
-			c=fgetc(dirty);
-			
-			if(c=='\n')fsm++;
-			else fsm=none;
-		
+			c=fgetc2(dirty);
+			if(c=='\n')
+				fsm++;
+			else if(c==EOF)
+				break;
+			else
+			       	fsm=none;
 		}
-		
 		fputc('\n',clean);
 		
-		
-		//feof fukcias poste kiamn la EOF legigxas ...
-		c=fgetc(dirty);
+		c=fgetc2(dirty);
 		if (c==EOF) break;
 		ungetc(c,dirty);
-		
-		
 	}
-
-
-
-
+	fclose(clean);
+	fclose(dirty);
+	printf("dosiero pura.srt estas kreita\n");
 	return 0;
-
-
 }
 
+//legu la vindozan newline-on (\r\n) kiel \n
+char fgetc2(FILE* file){
+	char c = fgetc(file);
+	if(c!= '\r' || !DIRTY_FILE_USES_WIN_NEWLINES) return c;
+	char nextc= fgetc(file);
+	if(nextc == '\n') return '\n';
+	ungetc(nextc,file);
+	return c;
 
-
-
-
+}
